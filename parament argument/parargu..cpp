55fa20d7eb945a17899c9c6c8d1ba076bf7e6135
DIM <4ebotarov_dim@inbox.ru>
2023-05-09 18:09:37 +0300

    edd 1

---
+++ parament argument/parargu..cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+ 
+ //void introduceMi (string name){
+ //	cout <<" My name is :"<<name<< endl;
+ //}
+  void introduceMi (string name,string cyti,int age){
+  	cout<<"My name is:"<<neme<<endl;
+  	cout<<"I from "<<syti<<endl;
+  	cout<<
+  }
+  
+  
+ int main() {
+// introduceMi("DIMA");
+	
+	
+	system("pause>0");
+}
