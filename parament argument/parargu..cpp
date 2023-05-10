1d1d1725183152fe890a8ded8adeb31d8ec5c3a2
chebotarevdmitr <127180093+chebotarevdmitr@users.noreply.github.com>
2023-05-11 00:26:30 +0300

    first

--- parament argument/parargu..cpp
+++ parament argument/parargu..cpp
@@ -2,18 +2,18 @@
 
 using namespace std;
  
- //void introduceMi (string name){
- //	cout <<" My name is :"<<name<< endl;
- //}
-  void introduceMi (string name,string cyti,int age){
-  	cout<<"My name is:"<<neme<<endl;
-  	cout<<"I from "<<syti<<endl;
-  	cout<<
-  }
+  void introduceMi (string name){
+ cout <<" My name is :"<<name<< endl;
+ }
+  //void introduceMi (string name,string cyti,int age){
+  //	cout<<"My name is:"<<neme<<endl;
+  //	cout<<"I from "<<syti<<endl;
+  //	cout<<
+ // }
   
   
  int main() {
-// introduceMi("DIMA");
+ introduceMi("DIMA");
 	
 	
 	system("pause>0");
