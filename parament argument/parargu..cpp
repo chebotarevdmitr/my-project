c7bd420a1931c65ec61ace08ac831002db070991
DIM <4ebotarov_dim@inbox.ru>
2023-05-12 00:35:59 +0300

    test

--- parament argument/parargu..cpp
+++ parament argument/parargu..cpp
@@ -2,14 +2,16 @@
 #include <string.h>
 using namespace std;
  //->( string name )<-parametr
-  void introduceMi (string name){
- cout <<" My name is :"<<name<< endl;
+  void introduceMi (string name,string sity,int age){
+ cout <<" My name is : "<<name<< endl;
+ cout <<"I am from : "<<sity<< endl;
+ cout <<" Age: "<<age<< "year old"<<endl;
  }
   
  int main() {
  // ->("DIMA")<-argument	
- introduceMi("DIMA");
- introduceMi("student");
+ introduceMi("DIMA","Sevastopol",45);
+ 
 	
-	system("pause>0");
+system("pause>0");	
 }
